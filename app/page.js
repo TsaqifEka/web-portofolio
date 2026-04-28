c5c4dbf51795bb1c97e4a905fb4f9dbffd63e528
TsaqifEka <tsaqifeka15@gmail.com>
2026-04-28 09:29:45 +0700

    tambah revalidate 0 agar real-time

--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
+// data agar real-time, tanpa cache
+export const revalidate = 0;
+
 // Setup Supabase
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
